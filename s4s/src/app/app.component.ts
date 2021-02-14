82cfacbf246e9a6bc9cb3b6c626e88181e20b8ec
Shaikh Saifuddin <shaikh.saifuddin@xebia.com>
2021-02-14 18:03:15 +0530

    f3 commit

--- s4s/src/app/app.component.ts
+++ s4s/src/app/app.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 's4s';
+export class AppComponent implements OnInit {
+  title = 'saif❤️sana';
+  currentTime = new Date();
+  seconds: any;
+  calDate: any;
+  minutes: any;
+  hours: any;
+  days: any;
+  findDiff() {
+    let d1 = Date.now();
+    let d2 = new Date(2021, 2, 28);
+    let temp = this.getDiff(d1, d2);
+    this.calDate = temp[0];
+    this.days = temp[1];
+    this.hours = temp[2];
+    this.minutes = temp[3];
+    this.seconds = temp[4];
+  }
+
+  getDiff(d1, d2) {
+    let secDiff = Math.floor((d2 - d1) / 1000);
+    let second = secDiff;
+    let minutes = Math.floor(second / 60);
+    let hours = Math.floor(minutes / 60);
+    let days = Math.floor(hours / 24);
+
+    hours = hours - days * 24;
+    minutes = minutes - days * 24 * 60 - hours * 60;
+    second = second - days * 24 * 60 * 60 - hours * 60 * 60 - minutes * 60;
+    let newDate =
+      'Days: ' +
+      days +
+      ' Hours: ' +
+      hours +
+      ' Minutes: ' +
+      minutes +
+      ' Seconds: ' +
+      second;
+    return [newDate, days, hours, minutes, second];
+  }
+  newTime = setInterval(() => {
+    this.currentTime = new Date();
+    this.findDiff();
+  }, 1000);
+
+  ngOnInit() {
+    this.findDiff();
+  }
 }
